2a2467889a1bfd6002b3ff68da8ec89d74f17860
David <david.lourenco94@gmail.com>
2022-08-17 20:25:06 -0300

    Segundo Requisito OK

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PlanetContext from '../context/PlanetContext';
 
 function Table() {
-  const { planets } = useContext(PlanetContext);
-  // console.log(planets);
+  const {
+    filterByName: { name },
+    setName,
+  } = useContext(PlanetContext);
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        const data = json.results;
+        for (let i = 0; i < data.length; i += 1) {
+          delete data[i].residents;
+        }
+        setPlanets(data);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredPlanets = planets.filter((element) => (
+    element.name.toLowerCase().includes(name)
+  ));
+
+  // console.log(filteredPlanets);
+
   return (
     <table>
+      <input
+        type="text"
+        placeholder="Filter"
+        data-testid="name-filter"
+        onChange={ (e) => setName(e.target.value) }
+      />
       <thead>
         <tr>
           <th>Name</th>
@@ -24,21 +60,21 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        {Object.keys(planets).map((keys) => (
-          <tr key={ keys }>
-            <td>{planets[keys].name}</td>
-            <td>{planets[keys].rotation_period}</td>
-            <td>{planets[keys].orbital_period}</td>
-            <td>{planets[keys].diameter}</td>
-            <td>{planets[keys].climate}</td>
-            <td>{planets[keys].gravity}</td>
-            <td>{planets[keys].terrain}</td>
-            <td>{planets[keys].surface_water}</td>
-            <td>{planets[keys].population}</td>
-            <td>{planets[keys].films}</td>
-            <td>{planets[keys].created}</td>
-            <td>{planets[keys].edited}</td>
-            <td>{planets[keys].url}</td>
+        {filteredPlanets.map((element, index) => (
+          <tr key={ index }>
+            <td>{element.name}</td>
+            <td>{element.rotation_period}</td>
+            <td>{element.orbital_period}</td>
+            <td>{element.diameter}</td>
+            <td>{element.climate}</td>
+            <td>{element.gravity}</td>
+            <td>{element.terrain}</td>
+            <td>{element.surface_water}</td>
+            <td>{element.population}</td>
+            <td>{element.films}</td>
+            <td>{element.created}</td>
+            <td>{element.edited}</td>
+            <td>{element.url}</td>
           </tr>
         ))}
       </tbody>
--- src/context/PlanetProvider.js
+++ src/context/PlanetProvider.js
@@ -1,33 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PlanetContext from './PlanetContext';
 
 function PlanetProvider({ children }) {
-  const [planets, setPlanets] = useState('');
+  const [planets, setPlanets] = useState([]);
+  const [name, setName] = useState('');
 
-  useEffect(() => {
-    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const json = await response.json();
-        const data = json.results;
-        for (let i = 0; i < data.length; i += 1) {
-          delete data[i].residents;
-        }
-        // console.log(data);
-        setPlanets(data);
-      } catch (error) {
-        console.log('error', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const contextValue = {
+    planets,
+    setPlanets,
+    filterByName: { name },
+    setName,
+  };
 
   return (
-    <PlanetContext.Provider value={ { planets, setPlanets } }>
+    <PlanetContext.Provider value={ contextValue }>
       { children }
     </PlanetContext.Provider>
   );
