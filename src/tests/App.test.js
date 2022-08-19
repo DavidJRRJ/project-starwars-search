4483bac364b0dcb4ae6bdeeef46f665d0d10451f
David <david.lourenco94@gmail.com>
2022-08-18 21:47:22 -0300

    Requisito 5 OK

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,39 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import PlanetProvider from '../context/PlanetProvider';
 
-// test('I am your test', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Hello, App!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('Teste do Projeto Starwars', () => {
+  beforeEach(() => {
+    render(
+      <PlanetProvider>
+        <App/>
+      </PlanetProvider>
+    )
+  });
+
+  it('Verifica o cabeçalho do componente Table', () => {
+    screen.getByRole('columnheader', {  name: /name/i});
+    screen.getByRole('columnheader', {  name: /rotation period/i});
+    screen.getByRole('columnheader', {  name: /orbital period/i});
+    screen.getByRole('columnheader', {  name: /diameter/i});
+    screen.getByRole('columnheader', {  name: /climate/i});
+    screen.getByRole('columnheader', {  name: /gravity/i});
+    screen.getByRole('columnheader', {  name: /terrain/i});
+    screen.getByRole('columnheader', {  name: /surface water/i});
+    screen.getByRole('columnheader', {  name: /population/i});
+    screen.getByRole('columnheader', {  name: /films/i});
+    screen.getByRole('columnheader', {  name: /created/i});
+    screen.getByRole('columnheader', {  name: /edited/i});
+    screen.getByRole('columnheader', {  name: /url/i});
+  });
+
+  it('Verifica os inputs do componente Table', () => {
+    screen.getByTestId('name-filter');
+    screen.getByTestId('column-filter');
+    screen.getByTestId('comparison-filter');
+    screen.getByTestId('value-filter');
+    screen.getByTestId('button-filter');
+    screen.getByTestId('button-remove-filters');
+  })
+})
